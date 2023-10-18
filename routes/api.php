c77c8767ebfa550963ace4607ecd7f98228c0e5c
AsoAfan <aso.ascs2021@uoh.edu.iq>
2023-10-18 17:20:05 +0300

    Bug fixes

--- routes/api.php
+++ routes/api.php
@@ -50,11 +50,6 @@ use Illuminate\Support\Facades\Route;
 
 //Route::middleware('cors')->group(function () {
 Route::middleware('json')->group(function () {
-    Route::get('test1', function () {
-        return ["test" => glob(__DIR__ . '/*/*.php'),
-
-        ];
-    })->name('test1'); // DEV
 
 
     // ADMIN
