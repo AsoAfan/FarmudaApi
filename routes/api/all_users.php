6c075f720900c29373f3eb7df18c44371d86be16
AsoAfan <aso.ascs2021@uoh.edu.iq>
2023-11-01 09:47:19 +0300

    Test

--- routes/api/all_users.php
+++ routes/api/all_users.php
@@ -1,2 +1,6 @@
 <?php
+
+use App\Http\Controllers\HadisController;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/hadis/latest', [HadisController::class, 'latest']); // read | 2 latest Hadises TODO: ALL_USERS
